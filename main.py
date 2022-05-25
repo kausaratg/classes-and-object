f860dc5523f76ad494ccf4824a96171030a632b2
kausaratg <kausaratg@gmail.com>
2022-05-26 01:26:31 +0200

    first push

---
+++ main.py
@@ -0,0 +1,27 @@
+class Student:
+    # [assignment] Skeleton class. Add your code here
+    def __init__(self, name, age, tracks,score):
+        self.name = name
+        self.age = int(age)
+        self.tracks = tracks
+        self.score = score
+
+    def change_name(self, name):
+        self.name = name
+    def change_age(self, age):
+        self.age = int(age)
+    def add_track(self, tracks):
+        self.tracks = tracks
+    def get_score(self):
+        return self.score
+        
+
+
+
+Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
+
+# Expected methods
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
